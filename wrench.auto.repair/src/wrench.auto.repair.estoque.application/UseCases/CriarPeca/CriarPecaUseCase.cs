29d8d1c8cf525374250e7fbffbf13a4bd9afa0e2
GSFZamai <gsfzamai@gmail.com>
2026-04-13 22:27:27 -0300

    inicio implementação domínio estoque

---
+++ wrench.auto.repair/src/wrench.auto.repair.estoque.application/UseCases/CriarPeca/CriarPecaUseCase.cs
@@ -0,0 +1,25 @@
+﻿using wrench.auto.repair.estoque.domain.Entities;
+using wrench.auto.repair.estoque.domain.Interfaces.Repositories;
+
+namespace wrench.auto.repair.estoque.application.UseCases.CriarPeca;
+
+public class CriarPecaUseCase
+{
+    IPecaRepository pecaRepository;
+
+    public CriarPecaUseCase(IPecaRepository pecaRepository)
+    {
+        this.pecaRepository = pecaRepository;
+    }
+
+    public void criarPeca(Peca peca)
+    {
+        pecaRepository.CriarPeca(peca);
+    }
+}
+
+
+public class Teste
+{
+    
+}
---
+++ wrench.auto.repair/src/wrench.auto.repair.estoque.domain/Entities/Peca.cs
@@ -0,0 +1,12 @@
+﻿namespace wrench.auto.repair.estoque.domain.Entities;
+
+public class Peca
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public string Nome { get; set; }
+    public string Descricao   {get; set; }
+    public double Quantidade { get; set; }
+    public double Valor { get; set; }
+    public DateTime DataCadastro { get; } = DateTime.Now;
+    public DateTime DataAlteracao { get; } = DateTime.Now;
+}
